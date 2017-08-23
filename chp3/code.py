f4c1c5df359f85077db6b655cc140febab7b1304
lromang <luis.roangarci@gmail.com>
2017-08-23 12:35:44 -0500

    ..

--- chp3/code.py
+++ chp3/code.py
@@ -12,7 +12,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model    import Perceptron
 from sklearn                 import preprocessing
 from sklearn.metrics         import accuracy_score
+from matplotlib.colors       import ListedColormap
 
+import matplotlib.pyplot as plt
 import numpy as np
 
 ## ----------------------------------------
@@ -61,3 +63,76 @@ ppn.fit(X_train_std, y_train)
 y_pred = ppn.predict(X_test_std)
 print('Misclassified samples: %d ' % (y_test != y_pred).sum())
 print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
+
+## ----------------------------------------
+## Plots
+## ----------------------------------------
+def plot_decision_regions(X, y,
+                          classifier,
+                          test_idx   = None,
+                          resolution = 0.02):
+    ## Setup markers
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors  = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap    = ListedColormap(colors[:len(np.unique(y))])
+    ## Decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2       = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                                 np.arange(x2_min, x2_max, resolution))
+    Z              = classifier.predict(np.array([xx1.ravel(),
+                                                  xx2.ravel()]).T)
+    Z              = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha = 0.4, cmap = cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+    ## Plot all samples
+    X_test, y_test = X[test_idx, :], y[test_idx]
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x      = X[y == cl, 0],
+                    y      = X[y == cl, 1],
+                    alpha  = 0.8,
+                    c      = cmap(idx),
+                    marker = markers[idx],
+                    label  = cl)
+    ## Highlight test samples
+    if test_idx:
+        X_test, y_test = X[test_idx, :], y[test_idx]
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    c         = '',
+                    alpha     = 1.0,
+                    linewidth = 1,
+                    marker    = 'o',
+                    s         = 55,
+                    label     = 'test set')
+
+## Stack together train n test
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined     = np.hstack((y_train, y_test))
+## Plot
+plot_decision_regions(X          = X_combined_std,
+                      y          = y_combined,
+                      classifier = ppn,
+                      test_idx   = range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc = 'upper left')
+plt.show()
+
+## ----------------------------------------
+## Logistic
+## ----------------------------------------
+def sigmoid(z):
+    return 1.0 / (1.0 + np.exp(-z))
+## Range of points
+z     = np.arange(-7, 7, 0.1)
+phi_z = sigmoid(z)
+plt.plot(z, phi_z)
+plt.axvline(0.0, color = 'k')
+plt.axhspan(0.0, 1.0, facecolor = '1.0', alpha = 1.0, ls = 'dotted')
+plt.yticks([0.0, 0.5, 1.0])
+plt.ylim(-0.1, 1.1)
+plt.xlabel('z')
+plt.ylabel('$\phi (z) $')
+plt.show()
